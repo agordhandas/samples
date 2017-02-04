0f0f0ea2d7d9240d5895188c97b617a26dd5eee9
agordhandas <agordhandas@gmail.com>
2017-02-04 10:28:35 -0800

    Update device-info.js

--- web-bluetooth/device-info.js
+++ web-bluetooth/device-info.js
@@ -23,7 +23,7 @@ function onButtonClick() {
   navigator.bluetooth.requestDevice({filters: filters})
   .then(device => {
     log('> Name:             ' + device.name);
-    log('> Properties:       ' + device);
+    log('> Properties:       ' + device.uuids);
     log('> Connected:        ' + device.gatt.connected);
   })
   .catch(error => {
